4aa3c1cce4054bf4ce81e4322f97f8bcc644d419
Minh Nguyen-Hue <minh.nh1989@gmail.com>
2019-07-03 17:32:53 +0700

    Use fn database_url instead constant to solve development errors
    # Summary

--- server/src/db.rs
+++ server/src/db.rs
@@ -1,4 +1,5 @@
 use std::ops::Deref;
+use std::env;
 use rocket::http::Status;
 use rocket::request::{FromRequest, self};
 use rocket::{Request, State, Outcome};
@@ -9,10 +10,13 @@ use r2d2_diesel::ConnectionManager;
 use diesel::pg::PgConnection;
 
 pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
-static DATABASE_URL: &'static str = env!("DATABASE_URL");
+
+fn database_url() -> String {
+    env::var("DATABASE_URL").expect("DATABASE_URL must be set")
+}
 
 pub fn connect() -> Pool {
-    let manager = ConnectionManager::<PgConnection>::new(DATABASE_URL);
+    let manager = ConnectionManager::<PgConnection>::new(database_url());
     r2d2::Pool::builder().build(manager).expect("Failed to create pool")
 }
 
