a4e52d7904d5e8beb2e063fd5777df748e370e93
Minh Nguyen-Hue <minh.nh1989@gmail.com>
2019-07-07 13:47:06 +0700

    Return both http status code and response data in json
    # Summary

--- server/src/books/handler.rs
+++ server/src/books/handler.rs
@@ -3,17 +3,20 @@ use crate::google_json_response::{ render_errors, render_records };
 use crate::models::Book;
 use rocket_contrib::json::{ Json, JsonValue };
 use rocket::response::status;
+use rocket::http::Status;
 use super::repository;
 use super::repository::InsertableBook;
 
 #[get("/")]
-pub fn list(connection: Connection) -> JsonValue {
-    render_records(repository::list(&connection))
+pub fn list(connection: Connection) -> Result<JsonValue, Status> {
+    repository::list(&connection)
+        .map(|x| render_records(x))
+        .map_err(|_e| Status::InternalServerError )
 }
 
 #[post("/", data = "<book>")]
-pub fn create(book: Json<InsertableBook>, connection: Connection) -> Result<status::Created<Json<Book>>, JsonValue> {
+pub fn create(book: Json<InsertableBook>, connection: Connection) -> Result<status::Created<Json<Book>>, status::Custom<JsonValue>> {
     repository::create(book.into_inner(), &connection)
         .map(|x| status::Created(String::from("/books"), Some(Json(x))) )
-        .map_err(|e| render_errors(e, String::from("422")))
+        .map_err(|e| render_errors(e, Status::UnprocessableEntity))
 }
--- server/src/google_json_response.rs
+++ server/src/google_json_response.rs
@@ -1,15 +1,20 @@
 use diesel;
 use serde::ser::Serialize;
 use rocket_contrib::json;
-use rocket_contrib::json::{  JsonValue };
+use rocket_contrib::json::JsonValue;
+use rocket::http::Status;
+use rocket::response::status::Custom;
 
-pub fn render_errors(err: diesel::result::Error, code: String) -> JsonValue {
-    json!({
-        "error": {
-            "code": code,
-            "message": err.to_string()
-        }
-    })
+pub fn render_errors(err: diesel::result::Error, status: Status) -> Custom<JsonValue> {
+    Custom(
+        status,
+        json!({
+            "error": {
+                "code": status.code,
+                "message": err.to_string()
+            }
+        })
+    )
 }
 
 pub fn render_records<T: Serialize>(records: Vec<T>) -> JsonValue {
