61d846f49f80f0c17481b833cd990424ef311ec2
Minh Nguyen-Hue <minh.nh1989@gmail.com>
2019-07-07 19:24:55 +0700

    Refactor db connection
    # Summary

--- server/src/db.rs
+++ server/src/db.rs
@@ -1,44 +1,4 @@
-use std::ops::Deref;
-use std::env;
-use rocket::http::Status;
-use rocket::request::{FromRequest, self};
-use rocket::{Request, State, Outcome};
+use rocket_contrib::databases::diesel;
 
-use r2d2;
-use r2d2_diesel::ConnectionManager;
-
-use diesel::pg::PgConnection;
-
-pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
-
-fn database_url() -> String {
-    env::var("DATABASE_URL").expect("DATABASE_URL must be set")
-}
-
-pub fn connect() -> Pool {
-    let manager = ConnectionManager::<PgConnection>::new(database_url());
-    r2d2::Pool::builder().build(manager).expect("Failed to create pool")
-}
-
-pub struct Connection(pub r2d2::PooledConnection<ConnectionManager<PgConnection>>);
-
-impl<'a, 'r> FromRequest<'a, 'r> for Connection {
-    type Error = ();
-
-    fn from_request(request: &'a Request<'r>) -> request::Outcome<Connection, ()> {
-        let pool = request.guard::<State<Pool>>()?;
-
-        match pool.get() {
-            Ok(conn) => Outcome::Success(Connection(conn)),
-            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ()))
-        }
-    }
-}
-
-impl Deref for Connection {
-    type Target = PgConnection;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
+#[database("diesel_postgres_pool")]
+pub struct Connection(diesel::PgConnection);
