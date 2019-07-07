6fa857e38bc2d51bf070f9a301a16ee2a0003a11
Minh Nguyen-Hue <minh.nh1989@gmail.com>
2019-07-07 11:04:06 +0700

    Add api to create book
    # Summary

--- server/src/books/handler.rs
+++ server/src/books/handler.rs
@@ -1,9 +1,20 @@
 use crate::db::Connection;
+use crate::google_json_response::render_errors;
+use crate::models::Book;
 use rocket_contrib::json;
 use rocket_contrib::json::{ Json, JsonValue };
+use rocket::response::status;
 use super::repository;
+use super::repository::InsertableBook;
 
 #[get("/")]
 pub fn list(connection: Connection) -> Json<JsonValue> {
     Json(json!(repository::list(&connection)))
+}
+
+#[post("/", data = "<book>")]
+pub fn create(book: Json<InsertableBook>, connection: Connection) -> Result<status::Created<Json<Book>>, JsonValue> {
+    repository::create(book.into_inner(), &connection)
+        .map(|x| status::Created(String::from("/books"), Some(Json(x))) )
+        .map_err(|e| render_errors(e, String::from("422")))
 }
--- server/src/models.rs
+++ server/src/models.rs
@@ -1,7 +1,7 @@
 use crate::schema::books;
 
 #[table_name = "books"]
-#[derive(Serialize, Deserialize, Queryable, Insertable)]
+#[derive(Serialize, Deserialize, Queryable, AsChangeset)]
 pub struct Book {
     pub id: i32,
     pub name: String,
