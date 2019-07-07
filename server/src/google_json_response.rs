0159893657a3cbb2973b7162e919d80ad47cbdc5
Minh Nguyen-Hue <minh.nh1989@gmail.com>
2019-07-07 11:17:24 +0700

    Use google_json_response::render_records for render list of records
    # Summary

--- server/src/google_json_response.rs
+++ server/src/google_json_response.rs
@@ -1,4 +1,5 @@
 use diesel;
+use serde::ser::Serialize;
 use rocket_contrib::json;
 use rocket_contrib::json::{  JsonValue };
 
@@ -7,6 +8,15 @@ pub fn render_errors(err: diesel::result::Error, code: String) -> JsonValue {
         "error": {
             "code": code,
             "message": err.to_string()
+        }
+    })
+}
+
+pub fn render_records<T: Serialize>(records: Vec<T>) -> JsonValue {
+    json!({
+        "data": {
+            "items": records,
+            "total_items": records.len()
         }
     })
 }
