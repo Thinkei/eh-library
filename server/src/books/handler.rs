9ab499de299741c453a9b95cdc8a7194efd456bf
Minh Nguyen-Hue <minh.nh1989@gmail.com>
2019-07-07 11:17:24 +0700

    Use google_json_response::render_records for render list of records
    # Summary

--- server/src/books/handler.rs
+++ server/src/books/handler.rs
@@ -1,15 +1,14 @@
 use crate::db::Connection;
-use crate::google_json_response::render_errors;
+use crate::google_json_response::{ render_errors, render_records };
 use crate::models::Book;
-use rocket_contrib::json;
 use rocket_contrib::json::{ Json, JsonValue };
 use rocket::response::status;
 use super::repository;
 use super::repository::InsertableBook;
 
 #[get("/")]
-pub fn list(connection: Connection) -> Json<JsonValue> {
-    Json(json!(repository::list(&connection)))
+pub fn list(connection: Connection) -> JsonValue {
+    render_records(repository::list(&connection))
 }
 
 #[post("/", data = "<book>")]
