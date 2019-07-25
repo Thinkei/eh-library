7b32a95c484e162d7bdde64e0dccd97a948b4313
Minh Nguyen-Hue <minh.nh1989@gmail.com>
2019-07-25 14:01:26 +0700

    Register a new account success

--- server/src/auth_repository.rs
+++ server/src/auth_repository.rs
@@ -1,40 +1,76 @@
-use crate::errors::{self, ServiceError};
+use crate::errors::ServiceError;
 use crate::models::{NewUser, User};
-use argonautica::{Hasher, Verifier};
+use argonautica::{input::Salt, Hasher, Verifier};
 use diesel::{self, prelude::*};
-#[derive(Deserialize)]
+
+#[derive(Debug, Clone, Deserialize)]
 pub struct AuthData {
-    email: String,
-    password: String,
+    pub email: String,
+    pub password: String,
+}
+
+lazy_static! {
+    pub static ref SECRET_KEY: String = std::env::var("SECRET_KEY").unwrap();
 }
 
 pub fn register(auth_data: AuthData, conn: &PgConnection) -> Result<User, ServiceError> {
-    // check duplicate email
-
     use crate::schema::users::dsl::{email, users};
 
-    let user: QueryResult<User> = users.filter(email.eq(auth_data.email)).first::<User>(conn);
+    let user: QueryResult<User> = users
+        .filter(email.eq(auth_data.email.clone()))
+        .first::<User>(conn);
 
     if let Ok(_) = user {
         return Err(ServiceError::EmailExisted);
     }
 
-    // let salt: String = generate_salt();
-    // let encrypted_password: String = encryt_password(password, salt);
+    let mut hasher: Hasher = Hasher::default()
+        .with_password(auth_data.password.as_str())
+        .with_secret_key(SECRET_KEY.as_str())
+        .to_owned();
 
-    Err(ServiceError::Unauthorized)
+    let encrypted_password = hasher
+        .hash()
+        .map_err(|err| {
+            dbg!(err);
+            ServiceError::Unauthorized
+        })
+        .unwrap();
+    let salt: Vec<u8> = hasher.salt().as_bytes().to_vec();
+
+    dbg!(encrypted_password.clone());
+    dbg!(salt.clone());
+
+    let new_user = NewUser {
+        email: auth_data.email,
+        encrypted_password: encrypted_password,
+        salt: salt,
+        is_admin: false,
+        created_at: chrono::Local::now().naive_local(),
+    };
+
+    dbg!(new_user.clone());
+
+    diesel::insert_into(users)
+        .values(&new_user)
+        .get_result(conn)
+        .map_err(|err| {
+            dbg!(err);
+            ServiceError::UnprocessableEntity
+        })
 }
 
-// pub fn encrypt_password(password: &str) -> Result<String, ServiceError> {
-//     Hasher::default()
-//         .with_password(password)
-//         .with_secret_key(SECRET_KEY)
-//         .hash()
-//         .map_err(|err| {
-//             dbg!(err);
-//             ServiceError::Unauthorized
-//         })
-// }
+pub fn encrypt_password(password: &str, salt: Salt) -> Result<String, ServiceError> {
+    Hasher::default()
+        .with_password(password)
+        .with_secret_key(SECRET_KEY.as_str())
+        .with_salt(salt)
+        .hash()
+        .map_err(|err| {
+            dbg!(err);
+            ServiceError::Unauthorized
+        })
+}
 
 // pub fn verify(hash: &str, password: &str) -> Result<bool, ServiceError> {}
 
@@ -43,7 +79,22 @@ mod tests {
     use super::*;
     use crate::test_helpers::*;
 
-    use std::env;
+    #[test]
+    fn register_when_success() {
+        let _guard = this_test_modifies_env();
+
+        let conn = connection();
+
+        let email: &str = "x@example.com";
+
+        let auth_data = AuthData {
+            email: email.to_owned(),
+            password: "123".to_owned(),
+        };
+
+        let user = register(auth_data, &conn).unwrap();
+        assert_eq!(user.email, email);
+    }
 
     #[test]
     fn register_when_duplicate_email() {
@@ -51,25 +102,10 @@ mod tests {
 
         let conn = connection();
 
-        let auth_data = AuthData {
-            email: "x@example.com".to_owned(),
-            password: "123".to_owned(),
-        };
-
-        let actual = register(auth_data, &conn);
-        assert_matches!(actual, Err(ServiceError::Unauthorized));
-    }
-
-    #[test]
-    fn register_when_success() {
-        let _guard = this_test_modifies_env();
-
-        let conn = connection();
-
         let new_user = NewUser {
             email: "x@example.com".to_owned(),
             encrypted_password: "123".to_owned(),
-            salt: "123".to_owned(),
+            salt: "123".as_bytes().to_owned(),
             is_admin: false,
             created_at: chrono::Local::now().naive_local(),
         };
@@ -77,7 +113,8 @@ mod tests {
         use crate::schema::users;
         diesel::insert_into(users::table)
             .values(&new_user)
-            .execute(&conn);
+            .execute(&conn)
+            .unwrap();
 
         let auth_data = AuthData {
             email: "x@example.com".to_owned(),
